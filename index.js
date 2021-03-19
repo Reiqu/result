f1f3acc8475ba8bb41cf911057ca4dc5813f51c3
Adrian G <cgdoerpen@gmail.com>
2021-03-19 20:59:41 +0100

    added function

---
+++ index.js
@@ -0,0 +1,31 @@
+function resultHandler(res, code, data) {
+  res.status(code);
+  res.json(data);
+}
+
+function result (output, res) {
+  switch (output.statusCode) {
+    case 500: {
+      console.error(output.statusMessage);
+      resultHandler(res, output.statusCode, output.data)
+      break;
+    }
+    case 400: {
+      console.warn(output.statusMessage);
+      resultHandler(res, output.statusCode, output.data)
+      break;
+    }
+    case 404: {
+      console.warn(output.statusMessage);
+      resultHandler(res, output.statusCode, output.data)
+      break;
+    }
+    case 200: {
+      console.log(output.statusMessage);
+      resultHandler(res, output.statusCode, output.data)
+      break;
+    }
+  }
+}
+
+module.exports = result
